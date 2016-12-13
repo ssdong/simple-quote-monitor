7823a51202e4feaa370b714c1a91e288eaaf91e4
SSDong <susudong5@gmail.com>
2016-12-13 02:32:05 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+simple-quote-monitor
+===========================
+A simple Go tool for monitoring stock quotes and sending desktop alerts when price drops or rises to certain amount
+
+
+How it works
+------------
+Tired keeping track of the stock quotes ? Let simple-quote-monitor come rescue. It will periodically check the quote for
+every 30 to 40 seconds and send desktop alerts when price drops or rises to certain amount.
+![Desktop alert](../master/example.png?raw=true)
+It scrapes google finance website and finds the price on it. Unfortunately, I didn't find great finance API and google also deprecated theirs so I am doing scraping instead. **However, when you use this tool, please keep in mind that the data from google finance are NOT real-time in all cases. Some stocks have real-time while many others have around 15 mins delay.** When you set the range of price you are watching, please consider this 15 mins delay. This might be addressed in the future if I find providers who have real-time stock quotes.
+
+Dependencies
+------------
+Currently, the notification only works on OSX Mountain Lion's or after with the notification center
+and you have to brew install [terminal-notifier](https://github.com/julienXX/terminal-notifier) for desktop alerts. Simply
+run
+```
+$ brew install terminal-notifier
+```
+and you are done.
+**This will be addressed in the future as I might embed the terminal-notifier in the source code so you do
+not have to install terminal-notifier by yourself. I will also add supports to Windows and Linux**
+
+Tips
+----
+If you want the desktop alert to stay until you clicks it you would have to change your notification preferences for
+terminal-notifier in notification centre as this [one](https://www.dropbox.com/s/n2kt0in8q6syiu6/Screenshot%202016-10-11%2014.27.27.png?dl=0)
+Otherwise, the notification will go away in 5 seconds and you might miss it. Unfortunately this is something you have to
+do it manually since OSX forces the notification to disappear within a certain amount of time.
---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/net/html"
+)
+
+var usage = `
+  Usage: simple-quote-monitor [options]
+
+  -se <stock exchange>     The code of stock exchange
+  -ss <stock symbol>       The unique series of letters of a security
+  -min <number>            The minimum price watching for
+  -max <number>            The maximum price watching for
+`
+
+func main() {
+	if len(os.Args) < 2 {
+		man()
+	}
+
+	var symbol string
+	var exchange string
+	var min float64
+	var max float64
+
+	flag.StringVar(&symbol, "ss", "", "The code of stock exchange")
+	flag.StringVar(&exchange, "se", "", "The series of letters of a security")
+	flag.Float64Var(&min, "min", -0.1, "The minimum price watching for")
+	flag.Float64Var(&max, "max", -0.1, "The maximum price watching for")
+
+	flag.Parse()
+
+	if symbol == "" || exchange == "" || min == -0.1 || max == -0.1 {
+		log.Fatalf("Please provide all necessary arguments\n %s\n", usage)
+	}
+
+	if min > max {
+		log.Fatalf("Please make sure the minimum price %f is smaller than the maximum price %f", min, max)
+	}
+
+	exchange = strings.ToUpper(exchange)
+	symbol = strings.ToUpper(symbol)
+	url := fmt.Sprintf("https://www.google.com/finance?q=%s:%s", exchange, symbol)
+
+	// Ping and get the price data in every 30 - 40 seconds
+	for {
+		// Get the response from google finance search
+		response, getError := http.Get(url)
+		if getError != nil {
+			log.Fatalln(getError)
+		}
+
+		// Parse the fetched html file into nodes
+		node, parseError := html.Parse(response.Body)
+		if parseError != nil {
+			log.Fatalln(parseError)
+		}
+
+		// Starting from this line, the logic is tailored to match the id
+		// on the google finance page. Right now, we are focusing on the
+		// following HTML snippet structure which contains the stock price
+		//
+		//<div id="price-panel" class="id-price-panel goog-inline-block">
+		//   <div>
+		//       <span class="pr">
+		//           <span id="ref_121280306767104_l">9.89</span>
+		//       </span>
+		//       <div class="id-price-change nwp">
+		//           <span class="ch bld">
+		//              <span class="chr" id="ref_121280306767104_c">-0.13</span>
+		//              <span class="chr" id="ref_121280306767104_cp">(-1.30%)</span>
+		//           </span>
+		//       </div>
+		//   </div>
+		// </div>
+		result := findNodes(node, "class", "pr")
+
+		// Notice, the Node type within package net/html is really referring to a node
+		// in the tree-based API for XML, the DOM. I.e. An opening tag, text or a comment
+		// is also a valid node. After we find the opening tag <span class="pr">, we need
+		// to further looking for the number within the its <span> child.
+
+		// This line of code is really specific for the above DOM structure. We are getting
+		// the number's text node. Notice, the trickest part is that the newline character
+		// after <span class="pr"> is also another valid text node. I.e.
+		// <span class="pr">
+		//    (A "\n" text node here)<span id="ref_121280306767104_l">(span element node)9.89(text node)</span>
+
+		// If the returned and parsed HTML file does not contain any node with the specified
+		// (name, value) pair, we assume the provided stock symbol is actually invalid
+		if len(result) == 0 {
+			log.Fatalf("The stock exchange code %s or the stock symbol %s is invalid\n", exchange, symbol)
+		}
+		priceNode := result[0].FirstChild.NextSibling.FirstChild
+		price, _ := strconv.ParseFloat(priceNode.Data, 64)
+
+		var title string
+		var message string
+		if price <= min {
+			title = "Price drop alert"
+			message = fmt.Sprintf("The stock %s's price is now $%f\n", symbol, price)
+		}
+		if price >= max {
+			title = "Price rise alert"
+			message = fmt.Sprintf("The stock %s's price is now $%f\n", symbol, price)
+		}
+		fmt.Printf(message)
+		alert := exec.Command("terminal-notifier", "-title", title, "-message", message)
+		alert.Run()
+		// Sleep randomly from 30 seconds to 40 seconds
+		time.Sleep(time.Duration((30000 + rand.Intn(11)*1000)) * time.Millisecond)
+	}
+}
+
+// This will print out the flag options
+func man() {
+	fmt.Printf("%s\n", usage)
+	os.Exit(0)
+}
+
+// Parse the node(and its children) and find nodes with the passed-in (name, value) pair
+func findNodes(parent *html.Node, name string, value string) []*html.Node {
+	var result []*html.Node = nil
+
+	// Check the attribute array on current node
+	attributes := parent.Attr
+	for _, attribute := range attributes {
+		if attribute.Key == name && attribute.Val == value {
+			result = append(result, parent)
+		}
+	}
+	// Run Depth-first Search on its children and then siblings
+	for child := parent.FirstChild; child != nil; child = child.NextSibling {
+		result = append(result, findNodes(child, name, value)...)
+	}
+	return result
+}
